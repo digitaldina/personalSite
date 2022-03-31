048a44487116b4667ca7fa8e8476b3d21e7bfd1a
digitaldina <dinaelhanan@gmail.com>
2022-03-31 08:58:09 -0400

    Update 2PX3-Demo.js

--- src/pages/2PX3-Demo.js
+++ src/pages/2PX3-Demo.js
@@ -1,1 +1,45 @@
 
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Logo from "../components/logo.js"
+import Header from  "../components/header.js"
+class BlogIndex extends React.Component {
+  render() {
+    return (
+
+      <div>
+        <br />
+        <br />
+        <br />
+     <Logo />
+     <Header />
+     </div>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+   allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {   
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
