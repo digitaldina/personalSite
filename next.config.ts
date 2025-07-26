813347ce822f02b4807bdc617ace9811c6e12cd9
root <root@Administrator.localdomain>
2025-07-26 20:40:14 +0400

    init

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+const isProd = process.env.NODE_ENV === 'production';
 
+module.exports = { basePath: isProd ? '/personalSite' : '', assetPrefix: isProd ? '/personalSite/' : '', images: { unoptimized: true,  }, };
 export default nextConfig;
