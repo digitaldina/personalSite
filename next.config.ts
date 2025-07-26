820af3f17e617e6aca476a7547761e674512d5b4
root <root@Administrator.localdomain>
2025-07-26 22:00:38 +0400

    fix for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  distDir: 'dist', 
+  images: {
+    unoptimized: true,
+  },
 };
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = { basePath: isProd ? '/' : '', assetPrefix: isProd ? '/' : '', images: { unoptimized: true,  }, };
-export default nextConfig;
+// module.exports = { basePath: isProd ? '/' : '', assetPrefix: isProd ? '/' : '', images: { unoptimized: true,  }, };
+// export default nextConfig;
+
+module.exports = nextConfig;
