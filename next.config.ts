6c4d37acd6b6062f20dfdda88755d27241459689
root <root@Administrator.localdomain>
2025-07-26 20:47:05 +0400

    try without relative path for github pages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-// const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
-// module.exports = { basePath: isProd ? '/personalSite' : '', assetPrefix: isProd ? '/personalSite/' : '', images: { unoptimized: true,  }, };
+module.exports = { basePath: isProd ? '/' : '', assetPrefix: isProd ? '/' : '', images: { unoptimized: true,  }, };
 export default nextConfig;
